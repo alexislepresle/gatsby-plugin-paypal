6de1946d247381b8f0c20965869f69fd0c05ea86
alexislepresle <contact@alexislepresle.com>
2021-03-22 10:42:09 +0100

    Error: Invalid hook call. Hooks can only be called inside of the body of a function component. This #1

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
 
-    if (!pluginOptions.clientId) {
+    if (!pluginOptions?.clientId) {
         return null;
     }
 
